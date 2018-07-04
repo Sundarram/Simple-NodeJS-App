00ba15a43ec186c6458829aea6df12a17c66c6a8
Sundar Ram Swaminathan <sundar.is1@gmail.com>
2018-07-04 15:23:52 -0400

    Push to trigger Jenkins Job

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Everyone! Jenkins CI Testing in progress...');
+  res.send('Hello Everyone! Trigger Jenkins Job...');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
