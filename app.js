0c56b54b91306c3960d4e6ba4e77dc31122082f1
Sundar Ram Swaminathan <sundar.is1@gmail.com>
2018-07-06 00:12:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Everyone! Trigger Successful... Jenkins works fine! ');
+  res.send('Hello Everyone! Jenkins Build trigger successful! ');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
